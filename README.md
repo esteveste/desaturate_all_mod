93af4107a6605bec0cf5547721022b2251671620
Bernardo Esteves <bernardo.esteves@tecnico.ulisboa.pt>
2019-04-07 12:36:19 +0100

    added auto desature on Night Time

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Copy this folder to the gnome-shell extension folders, typically under `.local/s
 Using a terminal, you can do the following sequence of commands :
 
 $ git clone https://github.com/laerne/desaturate_all.git
-$ mv desaturate_all ".local/share/gnome-shell/extensions/desaturate_all@nicolas.brack.mail.be"
+$ mv desaturate_all_mod "~/.local/share/gnome-shell/extensions/desaturate_all_mod@esteveste"
 
 Then restart gnome by pressing `ALT`+`F2`, then entering `r` and validating with `RETURN`.
 
 You can now enable the extension by browsing to https://extensions.gnome.org/local .
 
 
-
+Based on https://github.com/laerne/desaturate_all
--- extension.js
+++ extension.js
@@ -11,40 +11,60 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+'use strict';
+
 const Clutter = imports.gi.Clutter;
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
 
 let button;
 let extension_icon;
 let color_effect;
 
 function _toggleEffect() {
-    if ( Main.uiGroup.has_effects( color_effect ) ) {
-        Main.uiGroup.remove_effect( color_effect );
+    if (Main.uiGroup.has_effects(color_effect)) {
+        Main.uiGroup.remove_effect(color_effect);
     } else {
-        Main.uiGroup.add_effect( color_effect );
+        Main.uiGroup.add_effect(color_effect);
     }
 }
 
+function _timeEnable() {
+    if (!Main.uiGroup.has_effects(color_effect)) {
+        Main.uiGroup.add_effect(color_effect);
+    }
+}
 
 function init() {
     //Creation of button
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    extension_icon = new St.Icon({ icon_name: 'applications-graphics-symbolic',
-                                   style_class: 'system-status-icon' });
+    button = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true
+    });
+    extension_icon = new St.Icon({
+        icon_name: 'applications-graphics-symbolic',
+        style_class: 'system-status-icon'
+    });
     button.set_child(extension_icon);
 
     //Creation of effect
     color_effect = new Clutter.DesaturateEffect();
-    
+
     //Signal connection
     button.connect('button-press-event', _toggleEffect);
+
+    //Set to activate on time
+    var now = new Date();
+    var millisTill = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 21, 0, 0, 0) - now;
+    if (millisTill < 0) {
+        millisTill += 86400000; // it's after the hour, try again tomorrow.
+    }
+    Mainloop.timeout_add_seconds(millisTill, _timeEnable);
 }
 
 function enable() {
